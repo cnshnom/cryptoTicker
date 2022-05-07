60b321847a9e08973e6d7bb61f1070f55cf95d60
cnshnom <yupeiyu.cns.hn@gmail.com>
2022-05-07 19:33:33 +0200

    price and timestamp. public/images

--- app/controllers/ticker_controller.rb
+++ app/controllers/ticker_controller.rb
@@ -1,6 +1,27 @@
 require 'httparty'
+require 'json'
 
 class TickerController < ApplicationController
+
+    def home
+        results = HTTParty.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=2",
+            :headers =>{
+                "X-CMC_PRO_API_KEY" => "d0eea4e6-b070-4e1b-85e4-93b14d94e76f",
+                "Content-Type" => "application/json"
+            }
+            )
+            @message = results
+            data = JSON.parse(results&.body)
+            @btcprice = data["data"][0]["quote"]["USD"]["price"]            
+            @ethprice = data["data"][1]["quote"]["USD"]["price"]
+
+            @status = data["status"]["timestamp"]
+            
+    
+    end
+
+
+
     def eth     
         
         results = HTTParty.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=2&price_max=10000",
@@ -18,7 +39,9 @@ class TickerController < ApplicationController
                 "X-CMC_PRO_API_KEY" => "d0eea4e6-b070-4e1b-85e4-93b14d94e76f",
                 "Content-Type" => "application/json"
             }
-            )
-            @message = results
+        )
+        data = JSON.parse(results&.body)
+        price = data["data"][0]["quote"]["USD"]["price"]
+        @message = price
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index
-
+  root :to => "ticker#home"
+  get 'home', to:'ticker#home'
   get 'eth', to:'ticker#eth'
   get 'btc', to:'ticker#btc'
 
