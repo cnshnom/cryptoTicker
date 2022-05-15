f31e227844585cd8eb64360cddb65e428270649d
cnshnom <yupeiyu.cns.hn@gmail.com>
2022-05-15 18:00:14 +0200

    dynamic data

--- app/controllers/ticker_controller.rb
+++ app/controllers/ticker_controller.rb
@@ -1,10 +1,12 @@
 require 'httparty'
 require 'json'
 
+require 'bigdecimal'
+require 'bigdecimal/util'
 class TickerController < ApplicationController
 
     def home
-        results = HTTParty.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=2",
+        results = HTTParty.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=50",
             :headers =>{
                 "X-CMC_PRO_API_KEY" => "d0eea4e6-b070-4e1b-85e4-93b14d94e76f",
                 "Content-Type" => "application/json"
@@ -12,8 +14,12 @@ class TickerController < ApplicationController
             )
             @message = results
             data = JSON.parse(results&.body)
-            @btcprice = data["data"][0]["quote"]["USD"]["price"]            
+            @btcprice = data["data"][0]["quote"]["USD"]["price"].to_d            
             @ethprice = data["data"][1]["quote"]["USD"]["price"]
+            @dlistsymbol = data["data"].map { |obj| obj["symbol"] }
+            @dlistUSD = data["data"].map { |obj| obj["quote"]["USD"]["price"] }
+            @dlistBTC = data["data"].map { |obj| obj["quote"]["USD"]["price"].to_d/(@btcprice)}
+            
 
             @status = data["status"]["timestamp"]
             
