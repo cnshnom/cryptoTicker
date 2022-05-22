7110b88cb3182211ff94cc48e4a7e96a3ba0e709
cnshnom <yupeiyu.cns.hn@gmail.com>
2022-05-22 13:21:14 +0200

    digits

--- app/controllers/ticker_controller.rb
+++ app/controllers/ticker_controller.rb
@@ -5,6 +5,31 @@ require 'bigdecimal'
 require 'bigdecimal/util'
 class TickerController < ApplicationController
 
+    def get_price(price_float)
+        price_string = '%.10f' % price_float
+        price = price_string.to_i
+        digits = price_string.to_s.split('.')[1]
+        # get all digits until you have at least two significant ones (>0)
+        arraydigit = digits.chars
+        count = 0
+        found = false
+        length = arraydigit.length
+        for idx in 0...arraydigit.length
+            if(arraydigit[idx]!='0')
+                count = count + 1
+            end
+            if(count == 2)
+                length = idx+1
+                break
+            end
+        end
+        delimiter = "."
+        price = price.to_s
+        digits = digits[0,length].to_s
+        final = price +delimiter+ digits
+        return final
+    end
+
     def home
         results = HTTParty.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=50",
             :headers =>{
@@ -17,12 +42,11 @@ class TickerController < ApplicationController
             @btcprice = data["data"][0]["quote"]["USD"]["price"].to_d            
             @ethprice = data["data"][1]["quote"]["USD"]["price"]
             @dlistsymbol = data["data"].map { |obj| obj["symbol"] }
-            @dlistUSD = data["data"].map { |obj| obj["quote"]["USD"]["price"] }
-            @dlistBTC = data["data"].map { |obj| obj["quote"]["USD"]["price"].to_d/(@btcprice)}
-            @dlistETH = data["data"].map { |obj| obj["quote"]["USD"]["price"].to_d/(@ethprice)}
+            @dlistUSD = data["data"].map { |obj| get_price(obj["quote"]["USD"]["price"]) }
+            @dlistBTC = data["data"].map { |obj| get_price(obj["quote"]["USD"]["price"].to_d/(@btcprice))}
+            @dlistETH = data["data"].map { |obj| get_price(obj["quote"]["USD"]["price"].to_d/(@ethprice))}
 
-            @status = data["status"]["timestamp"]
-            
+            @status = Time.now().strftime("%d.%m.%Y  %H:%M:%S")
     
     end
 
